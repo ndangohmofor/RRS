15179988b0816e46af213efc21e3beb554f5ffe6
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-10-27 22:12:43 -0400

    formatted linkedlist file to conform with pycharm requirements

--- linkedlist.py
+++ linkedlist.py
@@ -1,19 +1,17 @@
 from node import Node
 
+
 class LinkedList:
     def __init__(self, value=None):
         self.head_node = Node(value)
 
-
     def get_head_node(self):
         return self.head_node
-
 
     def insert_beginning(self, new_value):
         new_node = Node(new_value)
         new_node.set_next_node(self.head_node)
         self.head_node = new_node
-
 
     def remove_node(self, value):
         current_node = self.head_node
@@ -28,8 +26,6 @@ class LinkedList:
                 else:
                     current_node = next_node
 
-
-
     def stringify_list(self):
         string_list = ""
         current_node = self.head_node
@@ -37,5 +33,3 @@ class LinkedList:
             string_list += str(current_node.value) + "\n"
             current_node = current_node.get_next_node()
         return string_list
-
-    
