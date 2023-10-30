7d4f9006d7931f15d89169983927a56de04c99f8
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-10-30 06:01:56 -0400

    added the while loop prompts that user to enter a selected food type

--- main2.py
+++ main2.py
@@ -1,7 +1,7 @@
-from welcome import welcome
 from linkedlist2 import LinkedList
 from restaurantData import restaurant_data
 from restaurantTypes import types
+from welcome import welcome
 
 
 def insert_food_types(food_types_list):
@@ -17,7 +17,8 @@ def insert_restaurant_data(restau_list, food_type_list):
         restaurant_data_sublist = LinkedList()
         for restaurant in restau_list:
             if restaurant[0] == food_type:
-                restaurant_data_sublist.insert_beginning(restaurant[0], restaurant[1], restaurant[2], restaurant[3], restaurant[4])
+                restaurant_data_sublist.insert_beginning(restaurant[0], restaurant[1], restaurant[2], restaurant[3],
+                                                         restaurant[4])
         restaurant_data_list.insert_beginning(restaurant_data_sublist)
     return restaurant_data_list
 
@@ -49,3 +50,37 @@ while len(selected_food_type) == 0:
         print(food_type)
 
     # if only one type of restaurant is found, ask the user if they would like to display this restaurant
+    if len(food_type_matches) == 1:
+        selected_input = str(input(
+            "\nThe only matching type for the specified input is " + food_type_matches[
+                0] + ". \nDo you want to look at " +
+            food_type_matches[0] + "restaurants?"
+                                   "Enter y for yes and n for no\n"
+        )).lower()
+        # if user selects yes, retrieve the restaurant data here
+        if selected_input == 'y':
+            selected_food_type = food_type_matches[0]
+            print("Selected Food Type: " + selected_food_type)
+            restaurant_list_head = my_restaurant_list.get_head_node()
+
+            while restaurant_list_head.get_next_node():
+                sublist_head = restaurant_list_head.get_food_type().get_next_node()
+
+                if sublist_head.get_food_type() == selected_food_type:
+                    while sublist_head.get_next_node():
+                        print("-----------------------------------------------------")
+                        print("Name: " + sublist_head.get_name())
+                        print("Price: " + sublist_head.get_price() + "/5")
+                        print("Rating: " + sublist_head.get_rating() + "/5")
+                        print("Address: " + sublist_head.get_address())
+                        print("-----------------------------------------------------\n")
+                        sublist_head = sublist_head.get_next_node()
+
+                restaurant_list_head = restaurant_list_head.get_next_node()
+
+            # Ask the customer if they would like to search for other types of restaurants
+            repeat_loop = str(input(
+                "\nDo you want to find other restaurants? Enter y for yes and n for no.\n"
+            )).lower()
+            if repeat_loop == 'y':
+                selected_food_type = ''
