34db7f6f16ac432c5a477aa86db9e8780621b5bc
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-10-30 05:45:04 -0400

    take the user input for food type and create a list of food types matching that user's input

--- main2.py
+++ main2.py
@@ -22,8 +22,30 @@ def insert_restaurant_data(restau_list, food_type_list):
     return restaurant_data_list
 
 
-my_food_list = insert_food_types(types)
+my_food_type_list = insert_food_types(types)
 my_restaurant_list = insert_restaurant_data(restaurant_data, types)
 
 welcome()
 
+selected_food_type = ""
+
+while len(selected_food_type) == 0:
+    user_input = str(input(
+        "\nWhat type of food would you like to eat?\nType the beginning of that food type and press enter to see if "
+        "it's here\n"
+    )).lower()
+
+    food_type_matches = []
+
+    food_type_list_head = my_food_type_list.get_head_node()
+
+    while food_type_list_head:
+        if str(food_type_list_head.get_food_type()).startswith(user_input):
+            food_type_matches.append(food_type_list_head.get_food_type())
+        food_type_list_head = food_type_list_head.get_next_node()
+
+    # print the list of matching food types
+    for food_type in food_type_matches:
+        print(food_type)
+
+    # if only one type of restaurant is found, ask the user if they would like to display this restaurant
